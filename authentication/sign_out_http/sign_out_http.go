98508f2b82c255423d97f4801d2f437945c48af3
Kenton Vizdos <kvizdos@protonmail.com>
2025-06-12 19:21:18 -0400

    New locksmith-sdk + rm launchpad cookie

--- authentication/sign_out_http/sign_out_http.go
+++ authentication/sign_out_http/sign_out_http.go
@@ -3,6 +3,8 @@ package sign_out
 import (
 	"net/http"
 	"time"
+
+	"github.com/kvizdos/locksmith/launchpad"
 )
 
 type SignOutHTTP struct{}
@@ -12,10 +14,15 @@ func (m SignOutHTTP) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	cookie := http.Cookie{Name: "token", Value: "", Expires: time.Unix(0, 0), HttpOnly: true, Secure: true, Path: "/"}
 	sessionExpiresAtCookie := http.Cookie{Name: "ls_expires_at", Value: "", Expires: time.Unix(0, 0), HttpOnly: false, Secure: true, Path: "/"}
 	oauthprovidercookie := http.Cookie{Name: "ls_oauth_provider", Value: "", Expires: time.Unix(0, 0), HttpOnly: false, Secure: true, Path: "/"}
+	launchpadcookie := http.Cookie{Name: "LaunchpadUser", Value: "", Expires: time.Unix(0, 0), HttpOnly: false, Secure: true, Path: "/"}
 
 	http.SetCookie(w, &cookie)
 	http.SetCookie(w, &sessionExpiresAtCookie)
 	http.SetCookie(w, &oauthprovidercookie)
+
+	if launchpad.IS_ENABLED {
+		http.SetCookie(w, &launchpadcookie)
+	}
 
 	http.SetCookie(w, &http.Cookie{Name: "magic", Value: "", Expires: time.Unix(0, 0), HttpOnly: false, Secure: true, Path: "/"})
 	http.SetCookie(w, &http.Cookie{Name: "sid", Value: "", Expires: time.Unix(0, 0), HttpOnly: false, Secure: true, Path: "/"})
